f39fe749d6e2132275ae7e756ef0392d6acbf04b
UJJWAL <usmishra2133@gmail.com>
2025-04-16 00:49:54 +0530

    deployment ready

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,7 @@ app.use('/messages',msgrouter)
 if(process.env.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname,'../Frontend/dist')));
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'../FronteNd','dist','index.html'));
+        res.sendFile(path.join(__dirname,'../Frontend','dist','index.html'));
     }); 
 
 }
