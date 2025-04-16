d6c6707e40b29b5b5a469b6221f724acb17f589a
UJJWAL <usmishra2133@gmail.com>
2025-04-16 13:55:20 +0530

    finally

--- Backend/index.js
+++ Backend/index.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === 'production') {
       res.sendFile(path.join(dirname, '/Frontend/dist/index.html'));
     });
   }
-  ff
+  
 // Start server
 server.listen(PORT, () => {
   connectDB();
